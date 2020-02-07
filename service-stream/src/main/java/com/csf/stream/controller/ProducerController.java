cd710de6153f59c2c47f86468d1a0731ae274130
chanToTo <38659554+chanToTo@users.noreply.github.com>
2020-02-07 21:39:17 +0800

    基础学习demo

--- service-stream/src/main/java/com/csf/stream/controller/ProducerController.java
+++ service-stream/src/main/java/com/csf/stream/controller/ProducerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * @author orangeC
  * @description
